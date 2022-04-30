c315bd55e21fb670fa3a01540aba1c6326b1ff67
franci301 <gregotti20@gmail.com>
2022-04-30 14:13:22 +0100

    home page styling

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -21,7 +21,9 @@ function HomePage() {
                 <div className="row justify-content-center">
                     <div className="col" id="leftImg">
                         <img id="left-home-img" src="https://images.unsplash.com/photo-1454923634634-bd1614719a7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlJTIwd2Fsa2luZ3xlbnwwfHwwfHw%3D&w=1000&q=80" alt="" />
-                        <button className='btn btn-dark' onClick={routeTest}>Start The Test</button>
+                        <div id='tag'>
+                            <button className='btn btn-dark' onClick={routeTest}>Start The Test</button>
+                        </div>
                     </div>
                     <div className="col" id="rightImg">
                         <p>Sample text goes here</p>
@@ -30,7 +32,7 @@ function HomePage() {
                 </div>
             </div>
             {/* <div id='footerTest'> */}
-                <Footer />
+            <Footer />
             {/* </div> */}
         </div>
     );
