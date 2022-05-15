a6414ddbc142038aaaf0b8a61ea100892005da22
Francesco Gregotti <francescogregotti@Francescos-MBP.cust.communityfibre.co.uk>
2022-05-15 12:06:31 +0100

    test add spacing

--- src/Layouts/testAd.js
+++ src/Layouts/testAd.js
@@ -1,5 +1,5 @@
 import {useNavigate} from 'react-router-dom';
-
+import '../Assets/css/testAd.css'
 function TestAd() {
 
     const navigate = useNavigate();
@@ -7,7 +7,7 @@ function TestAd() {
         navigate('/Test/*');
     }
     return (
-        <div>
+        <div id='testContainer'>
             <h5>Take our Archetypes of Power Discovery to get personalized product selection!</h5>
             <button className="btn btn-dark" onClick={routeTest}>Start Archetypes of Power Discovery</button>
         </div>
