bb92b53e2dcf39f90f522d8cb64d2a0529a25620
Lenard K <kemlenny@gmail.com>
2022-05-25 01:42:36 +0100

    homepage has basic responsability

--- src/Layouts/footer.js
+++ src/Layouts/footer.js
@@ -10,42 +10,44 @@ function footer() {
                     <input type="text" placeholder='Email'/>
                 </div>
                 <div className="d-inline-flex" id='middle-container'>
-                    <div>
-                        <h5>Customer Service</h5>
-                        <div>
-                            <p>Help and Contacts</p>
-                            <p>Terms and Conditions</p>
-                            <p>Privacy Policy</p>
-                            <p>Cookie Preferences</p>
-                            <p>Accessibility</p>
+                    <div className="row">
+                        <div className="col">
+                            <h5>Customer Service</h5>
+                            <div>
+                                <p>Help and Contacts</p>
+                                <p>Terms and Conditions</p>
+                                <p>Privacy Policy</p>
+                                <p>Cookie Preferences</p>
+                                <p>Accessibility</p>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <h5>Shipping & Returns</h5>
+                            <div>
+                                <p>Orders and Shipments</p>
+                                <p>Tracking Orders</p>
+                                <p>Returns and Refunds</p>
+                                <p>FAQs</p>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <h5>About ARKX</h5>
+                            <div>
+                                <p>Our Story</p>
+                                <p>Loyalty Building Program</p>
+                                <p>Discount Invite a Friend</p>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <h5>Connect with us</h5>
+                            <div>
+                                <p>Instagram</p>
+                                <p>TikTok</p>
+                                <p>Facebook</p>
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <h5>Shipping & Returns</h5>
-                        <div>
-                            <p>Orders and Shipments</p>
-                            <p>Tracking Orders</p>
-                            <p>Returns and Refunds</p>
-                            <p>FAQs</p>
-                        </div>
-                    </div>
-                    <div>
-                        <h5>About ARKX</h5>
-                        <div>
-                            <p>Our Story</p>
-                            <p>Loyalty Building Program</p>
-                            <p>Discount Invite a Friend</p>
-                        </div>
-                    </div>
-                    <div>
-                        <h5>Connect with us</h5>
-                        <div>
-                            <p>Instagram</p>
-                            <p>TikTok</p>
-                            <p>Facebook</p>
-                        </div>
-                    </div>
-                </div>
+                </div>      
             </div>
         </footer>
     );
